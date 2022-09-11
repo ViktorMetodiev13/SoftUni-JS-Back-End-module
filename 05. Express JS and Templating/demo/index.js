73c2d6a23454388476efb459c3863539829dfe77
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-11 14:18:40 +0300

    Testing methods

--- 05. Express JS and Templating/demo/index.js
+++ 05. Express JS and Templating/demo/index.js
@@ -6,4 +6,20 @@ app.get('/', (req, res) => {
     res.send(201, 'Hello, Express!');
 });
 
+app.get('/catalog', (req, res) => {
+    res.send('Catalog page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+})
+
+app.post('/catalog', (req, res) => {
+    res.send(201, 'Article created!');
+});
+
+app.all('*', (req, res) => {
+    res.send(404, 'Not Found!');
+});
+
 app.listen(3000, () => console.log('Server listening on port 3000'));
