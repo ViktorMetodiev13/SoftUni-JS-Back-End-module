fdd3d1c5c9c5105c0fb8b827d88daf254df74588
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-12 21:25:44 +0300

    Changes

--- 05. Express JS and Templating/HandleBars demo/server.js
+++ 05. Express JS and Templating/HandleBars demo/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const hbs = require('express-handlebars');
+const hbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('handlebars', hbs({
+app.engine('.hbs', hbs.engine({
     extname: '.hbs'
-}))
+}));
 
-app.set('view engine', 'handlebars');
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-    res.send('it works')
-})
+    const data = {
+        name: 'Peter',
+        age: 24,
+        items: [
+            'Link', 
+            'Wallet', 
+            'Bubblegum',
+            'Spare coins'
+        ]
+    }
+
+    res.render('home')
+});
 
 app.listen(3000, () => console.log('Server is listening on port 3000'));
