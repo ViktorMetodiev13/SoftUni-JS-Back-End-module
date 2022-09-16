01d996af696bb80e5cb33b85762bb249556ee763
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-16 21:39:00 +0300

    setup handlebars, static files and catalog page

--- 06. Workshop ExpressJS and Templating/index.js
+++ 06. Workshop ExpressJS and Templating/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const { catalog } = require('./controllers/catalog');
+
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world!</h1>')
-})
+app.engine('hbs', hbs({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.use('/static', express.static('static'));
+
+app.get('/', catalog);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
