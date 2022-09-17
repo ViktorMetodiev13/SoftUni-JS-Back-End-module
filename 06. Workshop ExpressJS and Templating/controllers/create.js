97693fe8699a84e2efaa8455a32a0020ebdc4943
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-17 16:38:06 +0300

    add cube functionality partly complete

--- 06. Workshop ExpressJS and Templating/controllers/create.js
+++ 06. Workshop ExpressJS and Templating/controllers/create.js
@@ -1,9 +1,19 @@
+const { create } = require("../models/storage");
+
 module.exports = {
     create: (req, res) => {
         res.render('create', {title:'Create Cube'});
         
     },
     post: (req, res) => {
+        let cube = {
+            name: req.body.name,
+            description: req.body.description,
+            imageUrl: req.body.nameimageUrl,
+            difficultyLevel: req.body.difficultyLevel
+        }
+        create(cube);
+        
         res.redirect('/');
     }
 }
