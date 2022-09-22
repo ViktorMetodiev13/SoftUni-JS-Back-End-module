33ca39dfa50c026cc6256fcaee594e0309de41c8
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-22 14:43:26 +0300

    minor changes

--- 07. NoSQL and MongoDB/demo/index.js
+++ 07. NoSQL and MongoDB/demo/index.js
@@ -1,8 +1,11 @@
 //const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 
+
+start();
+
 async function start() {
-    const connectionStr = 'mongodb://localhost:27017';
+    const connectionStr = 'mongodb://localhost:27017/testdb';
 
     const client = await mongoose.connect(connectionStr, {
         useNewUrlParser: true,
@@ -10,9 +13,45 @@ async function start() {
     });
 
     console.log('Database connected');
+
+    //defining the mongoose rules
+    const catSchema = new mongoose.Schema({
+        name: String,
+        color: String
+    });
+    //constructor
+    const Cat = mongoose.model('Cat', catSchema);
+
+
+    const personSchema = new mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        age: Number
+    });
+    const Person = mongoose.model('Person', personSchema);
+
+    personSchema.methods.sayHi = function() {
+        console.log(`My name is ${this.firstName} and I am ${this.age} years old.`);
+    }
+
+    const person1 = new Person({
+        firstName: 'Peter',
+        lastName: 'Jackson',
+        age: 34,
+    });
+    const person2 = new Person({
+        firstName: 'John',
+        lastName: 'Smith',
+        age: 29,
+    });
+    //await person1.save();
+    //await person2.save();
+
+    const people = await Person.find({});
+    console.log(people[model]);
+
 }
 
-start();
 /*
 client.connect((err) => {
     if (err != null) {
