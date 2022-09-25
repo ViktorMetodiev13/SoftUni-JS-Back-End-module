3db69e116c55b28a9998300e97f16455d4085547
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-09-25 12:50:33 +0300

    minor code improvements

--- 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/controllers/create.js
+++ 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/controllers/create.js
@@ -1,16 +1,16 @@
 module.exports = {
     create: (req, res) => {
-        res.render('create', {title:'Create Cube'});
+        res.render('create', { title: 'Create Cube' });
     },
     post: async (req, res) => {
         let cube = {
             name: req.body.name,
             description: req.body.description,
             imageUrl: req.body.imageUrl,
-            difficultyLevel: Number(req.body.difficultyLevel)
+            difficulty: Number(req.body.difficulty)
         }
         await req.storage.create(cube);
-        
+
         res.redirect('/');
     }
 }
