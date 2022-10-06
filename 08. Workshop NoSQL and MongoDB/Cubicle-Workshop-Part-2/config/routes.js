3ef1028f3e8be3fe4f4f4fc61c7c7fb78e9380c5
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-06 12:00:06 +0300

    combined all cube controllers in 1 file for better management

--- 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/config/routes.js
+++ 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/config/routes.js
@@ -1,25 +1,25 @@
 const { about } = require('../controllers/about');
-const { catalog } = require('../controllers/catalog');
-const { create, createPost } = require('../controllers/create');
-const { details, attach } = require('../controllers/details');
-const { notFound } = require('../controllers/notFound');
+const { attach } = require('../controllers/details');
 const { createAccessory, accessoryPost } = require('../controllers/accessory');
 const { login } = require('../controllers/login');
 const { register } = require('../controllers/register');
 
+const productController = require('../controllers/productController');
+
+const { notFound } = require('../controllers/notFound');
 
 module.exports = (app) => {
-    app.get('/', catalog);
+    app.get('/', (req, res) => res.redirect('/products'));
     app.get('/about', about);
-    app.get('/details/:id', details);
-    app.get('/create', create);
-    app.post('/create', createPost);
+
+    app.use('/products', productController);
 
     app.get('/accessory/create', createAccessory);
     app.post('/accessory/create', accessoryPost);
     app.get('details/:id/attach', attach);
-    app.get('/login', login)
-    app.get('/register', register)
+
+    app.get('/login', login);
+    app.get('/register', register);
 
     app.all('*', notFound);
 }
--- 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/index.js
+++ 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressConfig = require('./config/express');
 const databaseConfig = require('./config/database')
 const routesConfig = require('./config/routes');
+const cookieParser = require('cookie-parser');
 
 
 const { init: storage } = require('./services/storage');
