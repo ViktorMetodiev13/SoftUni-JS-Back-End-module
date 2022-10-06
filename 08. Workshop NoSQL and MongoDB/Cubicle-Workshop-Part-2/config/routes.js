cb4895d098e207a877bf7d2f7e7c2d4a792cad30
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-06 16:29:08 +0300

    register validations added

--- 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/config/routes.js
+++ 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/config/routes.js
@@ -1,6 +1,3 @@
-const { login } = require('../controllers/login');
-const { register } = require('../controllers/register');
-
 const productController = require('../controllers/productController');
 const accessoryController = require('../controllers/accessoryController');
 const homeController = require('../controllers/homeController');
@@ -11,10 +8,6 @@ module.exports = (app) => {
     app.use('/products', productController);
     app.use('/accessory', accessoryController);
     app.use('/auth', authController);
-
-    app.get('/login', login);
-    app.get('/register', register);
-    
 
     app.use('/', homeController);
 }
--- 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/controllers/authController.js
+++ 08. Workshop NoSQL and MongoDB/Cubicle-Workshop-Part-2/controllers/authController.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
 
 router.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', { title: 'Register' });
 });
+
+router.post('/register', async (req, res) => {
+    try {
+        req.auth.register(req.body);
+        res.redirect('/auth/register');
+    } catch (err) {
+        res.render('register', { title: 'Register', error: err.message });
+    }
+
+})
+
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+router.post('/login', (req, res) => {
+
+
+    res.redirect('/auth/login');
+})
 
 module.exports = router;
