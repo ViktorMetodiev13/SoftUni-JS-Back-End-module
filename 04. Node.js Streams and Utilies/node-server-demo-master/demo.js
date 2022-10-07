3337984ce7ac584568c0462ff04fec8769d400e3
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-07 16:41:30 +0300

    Project current base

--- 04. Node.js Streams and Utilies/node-server-demo-master/demo.js
+++ 04. Node.js Streams and Utilies/node-server-demo-master/demo.js
@@ -3,8 +3,8 @@ const fs = require('fs/promises');
 demo();
 
 async function demo() {
-    const data = fs.readFile('package.json');
-    console.log(data);
+    const data = await fs.readFile('./package.json');
+    console.log(data.toString());
 }
 
 console.log('hello world');
