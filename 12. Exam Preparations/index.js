460d78c1b9314e902142a571d5056b57ec490e1f
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-13 23:25:31 +0300

    tested whether it works

--- 12. Exam Preparations/index.js
+++ 12. Exam Preparations/index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('it works!')
+    res.send('it works!');
+    res.send('it works great!')
 })
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
