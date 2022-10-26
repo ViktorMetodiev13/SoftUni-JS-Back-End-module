e30aa1a70c203f9c9475968ae660a7e50934c84e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-26 20:05:03 +0300

    BookingUni pages and basic functions

---
+++ 12.1 Exam Preparation - 1/services/userService.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User')
+
+
+const JWT_SECRET = 'rqwedsfkdjeiwo';
+
+
+async function register(username, password) {
+    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+
+    if (existing) {
+        throw new Error('Username is taken');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        username,
+        hashedPassword
+    });
+
+    user.save();
+
+    // TODO see assignment if registration creates user session
+    const token = createSession(user);
+
+    return token;
+}
+
+async function login(username, password) {
+    const user = await User.findOne({username }).collation({ locale: 'en', strength: 2 });
+    if (!user) {
+        throw new Error('Incorrect username or password');
+    }
+
+    const hasMatch = await bcrypt.compare(password, user.hashedPassword);
+
+    if (hasMatch == false) {
+        throw new Error('Incorrect username or password');
+    }
+
+    const token = createSession(user);
+
+    return token;
+}
+
+async function createSession({ _id, username }) {
+    const payload = {
+        _id,
+        username,
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET);
+
+    return token;
+}
+
+async function verifyToken(token) {
+    return jwt.verify(token, JWT_SECRET)
+}
+
+module.exports = {
+    register,
+    login,
+    verifyToken
+}
