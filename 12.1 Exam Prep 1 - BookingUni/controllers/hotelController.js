e55588acab35434a9c9c5458fb0548618b222955
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-28 10:38:47 +0300

    Edit functionality added

--- 12.1 Exam Prep 1 - BookingUni/controllers/hotelController.js
+++ 12.1 Exam Prep 1 - BookingUni/controllers/hotelController.js
@@ -1,4 +1,4 @@
-const { create, getById } = require('../services/hotelService');
+const { create, getById, update } = require('../services/hotelService');
 const { parseError } = require('../util/parser');
 
 const hotelController = require('express').Router();
@@ -6,7 +6,7 @@ const hotelController = require('express').Router();
 
 hotelController.get('/:id/details', async (req, res) => {
     const hotel = await getById(req.params.id);
-    
+
     res.render('details', {
         title: 'Hotel Details',
         hotel
@@ -32,7 +32,7 @@ hotelController.post('/create', async (req, res) => {
         if (Object.values(hotel).some(v => !v)) {
             throw new Error('All fields are required');
         }
-        
+
         await create(hotel);
         res.redirect('/');
     } catch (err) {
@@ -47,10 +47,45 @@ hotelController.post('/create', async (req, res) => {
 hotelController.get('/:id/edit', async (req, res) => {
     const hotel = await getById(req.params.id);
 
+    if (hotel.owner != req.user._id) {
+        return res.redirect('/auth/login');
+    }
+
     res.render('edit', {
         title: 'Edit Hotel',
         hotel
     });
+});
+
+hotelController.post('/:id/edit', async (req, res) => {
+    const hotel = await getById(req.params.id);
+
+    if (hotel.owner != req.user._id) {
+        return res.redirect('/auth/login');
+    }
+
+    const edited = {
+        name: req.body.name,
+        city: req.body.city,
+        imageUrl: req.body.imageUrl,
+        rooms: Number(req.body.rooms),
+    }
+
+    try {
+        if (Object.values(edited).some(v => !v)) {
+            throw new Error('All fields are required');
+        }
+
+        await update(req.params.id, edited);
+        res.redirect(`/hotel/${req.params.id}/details`);
+    } catch (err) {
+
+        res.render('edit', {
+            title: 'Edit Hotel',
+            hotel: Object.assign(edited, { _id: req.params.id }),
+            errors: parseError(err)
+        });
+    }
 });
 
 module.exports = hotelController;
