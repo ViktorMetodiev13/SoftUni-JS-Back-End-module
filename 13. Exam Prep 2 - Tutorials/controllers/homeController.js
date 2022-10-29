eda77b31572a7b74212ea88e7e155d89eb2aa188
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-10-29 12:06:26 +0300

    Course templates updated

--- 13. Exam Prep 2 - Tutorials/controllers/homeController.js
+++ 13. Exam Prep 2 - Tutorials/controllers/homeController.js
@@ -1,15 +1,22 @@
+const { getAllByDate, getRecent } = require('../services/courseService');
+
 const homeController = require('express').Router();
 
-homeController.get('/', (req, res) => {
+homeController.get('/', async (req, res) => {
     let view;
+    let courses = [];
+
     if (req.user) {
         view = 'user-home';
+        courses = await getAllByDate();
     } else {
         view = 'guest-home';
+        courses = await getRecent();
     }
 
     res.render(view, {
         title: 'Home Page',
+        courses
     });
 });
 
--- 13. Exam Prep 2 - Tutorials/services/courseService.js
+++ 13. Exam Prep 2 - Tutorials/services/courseService.js
@@ -5,11 +5,16 @@ async function getAllByDate() {
     return Course.find({}).sort({ createdAt: 1 }).lean();
 }
 
+async function getRecent() {
+    return Course.find({}).sort({ userCount: -1}).limit(3).lean();
+}
+
 async function createCourse(course) {
     return Course.create(course);
 }
 
 module.exports = {
     getAllByDate,
-    createCourse
+    createCourse,
+    getRecent
 }
