61780ea1d57287bd0ff0749536bcae2cd4bb2554
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2022-11-03 22:39:24 +0200

    Changes

---
+++ 13.0 Exam Prep 2 - Tutorials/middlewares/session.js
@@ -0,0 +1,18 @@
+const { verifyToken } = require("../services/userService");
+
+module.exports = () => async (req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+        try {
+            const userData = await verifyToken(token);
+            req.user = userData;
+            res.locals.username = userData.username;
+        } catch (err) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return;
+        }
+    }
+
+    next();
+}
